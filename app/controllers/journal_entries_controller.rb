653391eed2653fd02b2a615cd0af886a73900c11
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-19 14:21:38 -0500

    Add create actions for mood and journal entry

---
+++ app/controllers/journal_entries_controller.rb
@@ -0,0 +1,24 @@
+class JournalEntriesController < ApplicationController
+  def create
+    journal_entries = JournalEntry.new(
+      user_id: params[:user_id],
+      title: params[:title],
+      content: params[:content],
+    )
+    # journalEntry.user_id = current_user.id
+    if journal_entries.save
+      render json: { message: "Journal entry created successfully" }, status: :created
+    else
+      render json: { errors: journal_entries.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  # def update_journal_entry
+  #   user = User.find(params[:id])
+  #   if user.update(journal_entry: params[:journal_entry])
+  #     render json: { message: "Journal entry updated successfully" }, status: :ok
+  #   else
+  #     render json: { errors: user.errors.full_messages }, status: :bad_request
+  #   end
+  #end
+end
---
+++ app/controllers/moods_controller.rb
@@ -0,0 +1,24 @@
+class MoodsController < ApplicationController
+  def create
+    mood = Mood.new(
+      user_id: params[:user_id],
+      mood_type: params[:mood_type],
+      mood_intensity: params[:mood_intensity],
+    )
+    # mood.user_id = current_user.id
+    if mood.save
+      render json: { message: "Mood created successfully" }, status: :created
+    else
+      render json: { errors: mood.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  # def update_mood
+  #   user = User.find(params[:id])
+  #   if user.update(mood: params[:mood])
+  #     render json: { message: "Mood updated successfully" }, status: :ok
+  #   else
+  #     render json: { errors: user.errors.full_messages }, status: :bad_request
+  #   end
+  # end
+end
