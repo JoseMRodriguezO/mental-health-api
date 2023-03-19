3c4d4ac7a79d3f8b5000d19037216546366f5524
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-19 16:01:12 -0500

    Add test for mood and journal entries

--- test/controllers/journal_entries_controller_test.rb
+++ test/controllers/journal_entries_controller_test.rb
@@ -1,4 +1,18 @@
 require "test_helper"
 
 class JournalEntriesControllerTest < ActionDispatch::IntegrationTest
+  test "create" do
+    assert_difference "JournalEntry.count", 1 do
+      post "/journal_entries.json", params: { user_id: User.first.id, title: "First Journal", content: "This is my first journal" }
+      assert_response 201
+    end
+  end
+
+  test "show" do
+    get "/journal_entries/#{JournalEntry.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["user_id", "title", "content", "created_at", "updated_at"], data.keys
+  end
 end
--- test/controllers/moods_controller_test.rb
+++ test/controllers/moods_controller_test.rb
@@ -1,4 +1,18 @@
 require "test_helper"
 
 class MoodsControllerTest < ActionDispatch::IntegrationTest
+  test "create" do
+    assert_difference "Mood.count", 1 do
+      post "/moods.json", params: { user_id: User.first.id, mood_type: "happy", mood_intensity: "low" }
+      assert_response 201
+    end
+  end
+
+  test "show" do
+    get "/moods/#{Mood.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["user_id", "mood_type", "mood_intensity", "created_at", "updated_at"], data.keys
+  end
 end
