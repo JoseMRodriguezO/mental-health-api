abce0fafc56ab34f5bc6e30eb12ef25b4c93073a
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-19 12:12:56 -0500

    Add test for controllers and sessions for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,19 @@ class UsersController < ApplicationController
     @users = User.all
     render :index
   end
+
+  def create
+    user = User.new(
+      email: params[:email],
+      date_of_birth: params[:date_of_birth],
+      gender: params[:gender],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation],
+    )
+    if user.save
+      render json: { message: "User created successfully" }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -8,4 +8,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal User.count, data.length
   end
+
+  test "create" do
+    assert_difference "User.count", 1 do
+      post "/users.json", params: { email: "test@test.com", date_of_birth: "01/01/2023", gender: "M", password: "password", password_confirmation: "password" }
+      assert_response 201
+    end
+  end
+
+
 end
