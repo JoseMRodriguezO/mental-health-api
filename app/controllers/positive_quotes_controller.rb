440c6f365c826a9b8f8a2606c47d5156013f72be
JoseMRodriguez <Josemiguelrodriguez89@gmail.com>
2023-03-26 17:11:10 -0500

    Positive quote action

--- app/controllers/positive_quotes_controller.rb
+++ app/controllers/positive_quotes_controller.rb
@@ -1,2 +1,9 @@
 class PositiveQuotesController < ApplicationController
+  require "httparty"
+
+  def quote
+    response = HTTParty.get("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en")
+    quote = response.body
+    render json: { quote: quote }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "/journal_entries" => "journal_entries#index"
   get "/moods" => "moods#index"
   get "/journal_entries/:id" => "journal_entries#show"
+  get "/positive_quotes" => "positive_quotes#quote"
 
   post "/moods" => "moods#create"
   post "/journal_entries" => "journal_entries#create"
